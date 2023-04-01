e2cc26b8bc6c9368c643106411c13445b8f15e2d
willcastl3 <willcastl3@outlook.com>
2023-04-01 16:07:28 -0500

    Bottom Nav Bar (Home, Journal, Calendar)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    JournalFragment journalFragment = new JournalFragment();
+    CalendarFragment calendarFragment = new CalendarFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.journal:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,journalFragment).commit();
+                        return true;
+                    case R.id.calendar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,calendarFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
